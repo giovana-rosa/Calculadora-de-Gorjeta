21e4c1aadd82e4315aa693eb5bf7fb937d5258a6
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2026-03-09 20:15:08 -0300

    feat: css do projeto

--- src/components/entradaComponent.vue
+++ src/components/entradaComponent.vue
@@ -35,7 +35,7 @@ function handleClick() {
 
 <template>
   <div class="panel">
-    <h2>Entradas</h2>
+    <h2>ENTRADAS</h2>
     <ul>
       <li>
         <p>Valor: R$</p>
@@ -53,36 +53,56 @@ function handleClick() {
       </li>
     </ul>
 
-    <button class="small-btn" @click="handleClick">Calcular</button>
+    <button class="small-btn" @click="handleClick">CALCULAR</button>
   </div>
 </template>
 
 <style scoped>
+
 .panel {
-  background: #fff;
-  border: 1px solid #ddd;
+  background: #e8daff;
+  border: 1px solid #ab9ac9;
+  border-radius: 7px;
   padding: 12px;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px;
 }
 
 .panel h2 {
   margin: 0 0 6px 0;
-  font-size: 13px;
+  font-size: 16px;
+  color: #673ab6;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.panel p {
+  margin: 0 0 6px 0;
+  font-size: 15px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: 600;
 }
 
 .input {
   width: 100%;
   padding: 6px;
-  font-size: 12px;
-  border: 1px solid #999;
-  margin-bottom: 6px;
+  font-size: 15px;
+  border: 1px solid #673ab6;
+  border-radius: 5px;
+  margin: 0 0 6px 5px;
+  background: #ece1ff;
 }
 
-.small-btn {
-  font-size: 11px;
-  padding: 4px 6px;
-  border: 1px solid #999;
-  background: #eee;
+.panel .small-btn {
+  font-size: 14px;
+  border: 1px solid #673ab6;
+  border-radius: 5px;
+  background: #673ab6;
   cursor: pointer;
+  color: #fff;
+  font-weight: 600;
+  display: block;
+  margin: 0 auto;
+  padding: 4px 6px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 
 .panel ul li{
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@ import EntradaComponent from "../components/entradaComponent.vue";
 import SaidaComponent from "../components/saidaComponent.vue";
 import { ref } from "vue";
 
+const step = ref(1);
+
 const bill = ref("");
 const tip = ref("");
 const people = ref("");
@@ -36,9 +38,16 @@ function calculate() {
 
 <template>
   <div class="app">
-    <h1 class="title">Calculadora de Gorjeta e Divisão</h1>
+  <h1 class="title">Calculadora de Gorjeta e Divisão</h1>
 
-    <div class="layout">
+  <!-- Parte 1: tela inicial -->
+    <div v-if="step === 1" class="page">
+      <h2 class="subtitle">Calcule sua gorjeta</h2>
+      <button @click="step = 2" class="aqui">AQUI</button>
+    </div>
+
+    <!-- Parte 2: tela de cálculo -->
+    <div v-if="step === 2" class="page">
       <EntradaComponent
         :bill="bill"
         :tip="tip"
@@ -48,15 +57,20 @@ function calculate() {
         @update:people="people = $event"
         @click="calculate"
       />
+      </div>
 
+      <div class="page">
       <SaidaComponent
+      v-if="calculated"
         :total="total"
         :tipValue="tipValue"
         :perPerson="perPerson"
         :message="message"
         :calculated="calculated"
       />
-    </div>
+      </div>
+      
+    
   </div>
 </template>
 
@@ -78,13 +92,35 @@ body {
 }
 
 .title {
-  font-size: 18px;
-  margin: 0 0 12px 0;
+  font-size: 1.7rem;
+  margin: 2px 0 60px 0;
+  text-align: center;
+  color: #673ab6;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 
-.layout {
+.subtitle {
+  font-size: 1.3rem;
+  margin: 0 0 5px 0;
+  padding: 5vw 0 0 0;
+  text-align: center;
+  color: #673ab6;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.page {
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 18px;
+  gap: 30px;
+  padding-bottom: 30px;
+}
+.aqui {
+  display: block;
+  margin: 0 auto;
+  padding: 10px 20px;
+  font-size: 25px;
+  color: #fff;
+  background: #673ab6;
+  border: none;
+  border-radius: 7px;
+  cursor: pointer;
 }
 </style>
